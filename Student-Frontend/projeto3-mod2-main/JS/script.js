004ae148cc5fcc61095834186371b872226fe61f
MateusL21 <mateusleonel27@gmail.com>
2024-07-08 14:25:22 -0300

    projeto pausado para a correcao

--- Student-Frontend/projeto3-mod2-main/JS/script.js
+++ Student-Frontend/projeto3-mod2-main/JS/script.js
@@ -1,18 +1,41 @@
+$("#inputPhone").mask("(99)9 9999-9999");
+
 //data
 var products = [];
-
 var categories = [];
 
-//Onload
+loadCategories();
+loadStudents();
+    
+function loadCategories(){
+  $.ajax({
+    url:"http://localhost:8080/courses",
+    type:"GET",
+    async: false,
+    success: (response) =>{
+    categories = response
+    for(var cat of categories){
+      document.getElementById("selectCourse").innerHTML += `<option value=${cat.id}>${cat.name}</option>`;
+    }
+  }});
 
-//LoadCategories
+}
 
+      function loadStudents() {
 
+        $.getJSON("http://localhost:8080/students", (response) => {
+          students =response
 
-// save a student
+          for (let stud of students) {
+              addNewRow(stud);
+          }
+        });
+          
+      }
+
 function save() {
-    var prod = {
-        id: products.length + 1,
+    var stud = {
+        id: students.length + 1,
         name: document.getElementById("inputName").value,
         email: document.getElementById("inputEmail").value,
         phone: document.getElementById("inputPhone").value,
@@ -20,10 +43,16 @@ function save() {
         period: getSelectedShift()
     };
 
-    addNewRow(prod);
-    products.push(prod);
-
-    document.getElementById("formStudent").reset();
+    $.ajax({
+      url:"http://localhost:8080/students",
+      type:"POST",
+      contentType: "application/json",
+      data: JSON.stringify(stud),
+      success: (savedStudent) =>{
+        addNewRow(savedStudent);
+        student.push(savedStudent);
+        document.getElementById("formStudent").reset();
+    }});
 }
 
 function getSelectedShift() {
@@ -38,44 +67,42 @@ function getSelectedShift() {
     return "";
 }
 
-
-// Add new row
-function addNewRow(prod) {
+function addNewRow(stud) {
     var table = document.getElementById("studentsTable");
 
     var newRow = table.insertRow();
 
     // Insert ID
     var idCell = newRow.insertCell();
-    var idText = document.createTextNode(prod.id);
+    var idText = document.createTextNode(stud.id);
     idCell.appendChild(idText);
 
     // Insert Name
     var nameCell = newRow.insertCell();
-    var nameText = document.createTextNode(prod.name);
+    var nameText = document.createTextNode(stud.name);
     nameCell.appendChild(nameText);
 
     // Insert Email
     var emailCell = newRow.insertCell();
-    var emailText = document.createTextNode(prod.email);
+    var emailText = document.createTextNode(stud.email);
     emailCell.appendChild(emailText);
     emailCell.classList.add("d-none", "d-md-table-cell");
 
     // Insert Phone
     var phoneCell = newRow.insertCell();
-    var phoneText = document.createTextNode(prod.phone);
+    var phoneText = document.createTextNode(stud.phone);
     phoneCell.appendChild(phoneText);
     phoneCell.classList.add("d-none", "d-md-table-cell");
 
     // Insert Course
     var courseCell = newRow.insertCell();
-    var courseText = document.createTextNode(categories[prod.idCurso - 1].name);
+    var courseText = document.createTextNode(categories[stud.idCurso - 1].name);
     courseCell.appendChild(courseText);
     courseCell.classList.add("d-none", "d-md-table-cell")
 
     // Insert Shift
     var shiftCell = newRow.insertCell();
-    var shiftText = document.createTextNode(prod.period);
+    var shiftText = document.createTextNode(stud.period);
     shiftCell.appendChild(shiftText);
     shiftCell.classList.add("d-none", "d-md-table-cell");
 }
